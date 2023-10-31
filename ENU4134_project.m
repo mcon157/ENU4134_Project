ce3307c0347efb70cad74db01b7bc080d03e8bbf
mcon157 <139496152+mcon157@users.noreply.github.com>
2023-10-31 19:03:18 -0400

    Update ENU4134_project.m

--- ENU4134_project.m
+++ ENU4134_project.m
@@ -1,8 +1,9 @@
 clc; clear;
 
-kc = 15; %W/m*K
-emisf = 1;
-emisc = 1;
+g = 9.81;
+k_c = 15; %W/m*K
+emis_f = 1;
+emis_c = 1;
 %Importing benchmark
 open1 = fopen('benchmark_1.txt','r');
 benchmark1 = fscanf(open1,'%f');
@@ -36,18 +37,20 @@ while 1
         mdot =  benchmark1(6); %kg/s
         Pnom =  benchmark1(7); %MPa
         qlinmax =  benchmark1(8); %W/m
-        Dci =  benchmark1(9);
-        Dfo =  benchmark1(10);
+        D_ci =  benchmark1(9);
+        D_fo =  benchmark1(10);
         prompt2 = 'Is there Subcooled Boiling? [Y/N]';
         yn = input(prompt2,'s');
         while 1
             if strcmp("Y",yn)
                 fprintf("Selected Benchmark 1 with Subcooled Boiling")
-                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,Dci,Dfo)
+                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,D_ci,D_fo)
+                sub = 1;
                 break
             elseif strcmp("N",yn)
                 fprintf("Selected Benchmark 1 without Subcooled Boiling")
-                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,Dci,Dfo)
+                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,D_ci,D_fo)
+                sub = 0;
                 break
             else
                 fprintf("Please enter Y or N")
@@ -66,18 +69,20 @@ while 1
         mdot =  benchmark2(6); %kg/s
         Pnom =  benchmark2(7); %MPa
         qlinmax =  benchmark2(8); %W/m
-        Dci =  benchmark2(9);
-        Dfo =  benchmark2(10);
+        D_ci =  benchmark2(9);
+        D_fo =  benchmark2(10);
         prompt2 = 'Is there Subcooled Boiling? [Y/N]';
         yn = input(prompt2,'s');
         while 1
             if strcmp("Y",yn)
                 fprintf("Selected Benchmark 2 with Subcooled Boiling")
-                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,Dci,Dfo)
+                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,D_ci,D_fo)
+                sub = 1;
                 break
             elseif strcmp("N",yn)
                 fprintf("Selected Benchmark 2 without Subcooled Boiling")
-                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,Dci,Dfo)
+                fprintf("\nL = %f\nLe = %f\nD = %f,\nP = %f\nTm_in(C) = %f\nmdot = %f\nPnom = %f\nqlinmax = %f\nDci = %f\nDfo = %f",L,Le,D,P,Tm_inC,mdot,Pnom,qlinmax,D_ci,D_fo)
+                sub = 0;
                 break
             else
                 fprintf("Please enter Y or N")
@@ -99,17 +104,28 @@ z = linspace(inlet,outlet,400);
 h_feb = zeros(400,1);
 h_feb1 = h_f(Tm_inC);
 T_m = zeros(400,1);
-table = table(zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),'VariableNames',{'Cell','z','T_m','T_co','T_ci','T_fo','T_max','x','x_e','CHFR','dP (in this cell)'});
+T_co = zeros(400,1);
+T_ci = zeros(400,1);
+vol_l = zeros(400,1);
+vol_v = zeros(400,1);
+dP = zeros(400,1);
+T_fo = zeros(400,1);
+T_max = zeros(400,1);
+x_e = zeros(400,1);
+x = zeros(400,1);
+ZD = zeros(400,1);
+CHFR = zeros(400,1);
+boiling = 0;
+table = table(zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),zeros(400,1),'VariableNames',{'Cell','z','T_m','T_co','T_ci','T_fo','T_max','x','x_e','CHFR','dP'});
 
 for i = 1:400
+    %Energy balance and T_m
     table.Cell(i) = i;
     z_i = z(i);
     table.z(i) = z_i;
-    del_z = (abs(inlet)+outlet)/400;
-    %del_z = L/400;
-    z_half = z_i + 0.5*(del_z);
+    del_z = L/400;
+    z_half = z_i - 0.5*(del_z);
     q_lin = qlinmax*cos((pi*z_i)/Le);
-    %q_linhalf1 = qlinmax*cos((pi*z(1))/Le);
     q_linhalf = qlinmax*cos((pi*z_half)/Le);
     if i == 1 
         h_feb(i) = h_feb1 + (q_linhalf*(del_z))/mdot;
@@ -118,6 +134,146 @@ for i = 1:400
     end
     T_m(i) = interp1(h_f,Tsat,h_feb(i),'linear','extrap');
     table.T_m(i) = T_m(i);
+    h_fsat = interp1(Psat,h_f,(Pnom*10^6),'linear','extrap');
+    h_gsat = interp1(Psat,h_g,(Pnom*10^6),'linear','extrap');
+    T_sat = interp1(Psat,Tsat,(Pnom*10^6),'linear','extrap');
+    %T_co
+    Dh = D*((4/pi)*((P/D)^2)-1);
+    A = (P^2) - (pi/4)*(D^2);
+    G = mdot/A;
+    mu_l = interp1(Tsat,mu_f,T_m(i),'linear','extrap');
+    Re = (G*Dh)/mu_l;
+    Phi = 1.826*(P/D) - 1.0430;
+    Pr_l = interp1(Tsat,Pr_f,T_m(i),'linear','extrap');
+    Nu = Phi*(0.023*(Re^0.8)*(Pr_l^0.333));
+    k_l = interp1(Tsat,k_f,T_m(i),'linear','extrap');
+    htc = Nu*(k_l/Dh);
+    R_co = D/2;
+    hfg = interp1(Tsat,h_g,T_m(i),'linear','extrap') - interp1(Tsat,h_f,T_m(i),'linear','extrap');
+    if T_m(i) < T_sat
+        T_co(i) = T_m(i)+(q_lin/(2*pi*R_co*htc));
+        table.T_co(i) = T_co(i);
+    elseif T_m(i) >= T_sat
+         boiling = 1; 
+         x_e(i) = (h_feb(i)-h_fsat)/(h_gsat-h_fsat);
+         if sub == 0
+            x(i) = x_e(i);
+            table.x(i) = x(i);
+            table.x_e(i) = x_e(i);
+         elseif sub == 1 
+            if T_co(i) > T_sat 
+                ZD(i) = z_i;
+            end
+            if h_feb < h_fsat
+                x_e(i) = (h_feb-h_fsat)/(h_gsat-h_fsat);
+                ZD_1 = find(ZD>0,1,'first');
+                x_eZD = x_e(ZD_1);
+                x(i) = x_e(i)-x_eZD*exp((x_e(i)/x_eZD)-1);
+                table.x(i) = x(i);
+                table.x_e(i) = x_e(i);
+            end
+         end
+         %Schrock and Grossman
+         vol_l(i) = interp1(Tsat,vol_f,T_m(i),'linear','extrap');
+         rho_l = 1/vol_l(i);
+         vol_v(i) = interp1(Tsat,vol_g,T_m(i),'linear','extrap');
+         rho_v = 1/vol_v(i);
+         mu_l = interp1(Tsat,mu_f,T_m(i),'linear','extrap');
+         mu_v = interp1(Tsat,mu_g,T_m(i),'linear','extrap');
+         Xtt = (((1-x(i))/(x(i)))^0.9)*((rho_v/rho_l)^0.5)*((mu_l/mu_v)^0.1);
+         qll = q_lin/(2*pi*R_co);
+         htc2 = htc*(7400*(qll/(G*hfg))+(1.11*Xtt^(-0.66)));
+         T_co(i) = T_m(i)+(q_lin/(2*pi*R_co*htc2));
+         table.T_co(i) = T_co(i);
+    end
+
+    %Bowring
+    if boiling == 1
+        pr = 0.145*Pnom; %dont have to worry about pr < 1
+        n = 0.5*pr;
+        F1 = (pr^(-0.368))*exp(0.648*(1-pr));
+        F2 = F1*(pr^(-0.448)*exp(0.245*(1-pr)))^-1;
+        F3 = pr^(0.219);
+        F4 = F3*(pr^1.649);
+        if strcmp("BWR",type)
+            pr = 1;
+            F1 = 1;
+            F2 = 1;
+            F3 = 1;
+            F4 = 1;
+        end
+        A = (2.317*((hfg*Dh*G)/4)*F1)/(1+0.0143*F2*G*sqrt(Dh));
+        B = (G*Dh)/4;
+        C = (0.077*F3*Dh*G)/(1+0.347*F4*(G/1356)^n);
+        qcr = (A - B*hfg*x(i))/C; %in W/m^2
+        CHFR(i) = qcr/qll;
+        table.CHFR(i) = CHFR(i);
+    end
+    %Pressure Drop
+    f_lo = (Re^-0.18)*(0.1339+(0.09059*((P/D)-1))+(-0.09926*((P/D)-1)^2));
+    %Single-phase, no acceleration loss:
+    if boiling == 0
+        vol_l(i) = interp1(Tsat,vol_f,T_m(i),'linear','extrap');
+        rho_l = 1/vol_l(i);
+        dPg = g*rho_l;
+        dP(i) = ((f_lo*(1/Dh)*((G^2)/(2*rho_l))+dPg)*del_z);
+        table.dP(i) = dP(i);
+    %Two-phase with acceleration loss: HEM
+    elseif boiling == 1
+        alpha = 1/(1+((1-x(i))/x(i))*(rho_v/rho_l));
+        rho_m = alpha*rho_v + (1-alpha)*rho_l;
+        dPg = g*rho_m; %assuming theta = 0
+        volfg = interp1(Tsat,vol_g,T_m(i),'linear','extrap') - interp1(Tsat,vol_f,T_m(i),'linear','extrap');
+        del_x = x(i) - x(i-1);
+        dPa = (G^2)*(del_x/del_z)*volfg;
+        dP(i) = ((f_lo*(1/Dh)*((G^2)/(2*rho_l))+dPa+dPg))*del_z;
+        table.dP(i) = dP(i);
+    end
+    %T_ci
+    R_ci = D_ci/2;
+    T_ci(i) = T_co(i) + (q_lin/(2*pi*k_c))*log(R_co/R_ci);
+    table.T_ci(i) = T_ci(i);
+
+    %Numerical Method for Gap Conductance
+    R_fo = D_fo/2;
+    j = 1;
+    htc_g = 5000;
+    T_fo_o = 0;
+    T_fo_n = T_ci(i) + q_lin/(pi*(R_ci+R_fo)*htc_g);
+    while j == 1
+        T_fo_nK = T_fo_n + 273.15;
+        T_ciK = T_ci(i) + 273.15;
+        T_avg = (T_fo_nK+T_ciK)/2;
+        k_g = 15.8*(10^-4)*(T_avg^0.79);
+        gap_eff = R_ci-R_fo;
+        htc_g = k_g/gap_eff + 5.67*(10^-8)*(((T_fo_nK^4)-(T_ciK^4))/(T_fo_nK-T_ciK));%Emissivity part cancels to 1
+        T_fo_n = T_ci(i) + q_lin/(pi*(R_ci+R_fo)*htc_g);
+        compare = abs(T_fo_n-T_fo_o);
+        if compare < 0.001
+            T_fo(i) = T_fo_n;
+            j = 0; 
+        else
+            T_fo_o = T_fo_n;
+        end
+    end
+    table.T_fo(i) = T_fo(i);
+    
+    %Numerical Method for Fuel Centerline Temperature
+    k_bar = 3;
+    T_max_n = T_fo(i) + q_lin/(4*pi*k_bar);
+    k = 1;
+    while k == 1
+        kdT_max = 3824*log(402.4+T_fo(i))+((6.1256*10^-11)/4)*((T_fo(i)+273)^4)+q_lin/(4*pi);
+        kdT_check = 3824*log(402.4+T_max_n)+((6.1256*10^-11)/4)*((T_max_n+273)^4);
+        T_max_o = T_max_n;
+        T_max_n = T_max_o + (kdT_max-kdT_check)/100;
+        compare = abs(kdT_check-kdT_max);
+        if compare < 0.0001
+            T_max(i) = T_max_n;
+            k = 0;
+        end
+    end
+    table.T_max(i) = T_max(i);
 end
 filename = 'ENU_4134_Project.xlsx';
 if strcmp("PWR",type) && strcmp("N",yn)
